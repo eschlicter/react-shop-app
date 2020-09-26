7d763dee8a824aa4c9f7ee419681179b4726eab4
eschlicter <emily.schlicter@gmail.com>
2020-09-26 15:24:31 -0500

    added initial header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assests/terrapin4.svg'
+import './header.styles.scss';
+
+const Header = () => (
+    <div className='header'>
+        <div className='logo-container'>
+            <Link className='logo-container' to='/'>
+                <Logo className='logo' />
+            </Link>
+        </div>
+        <div className='options'>
+            <Link className='option' to='/shop'>SHOP</Link>
+            <Link className='option' to='/contact'>CONTACT</Link>
+        </div>
+
+    </div>
+)
+
+export default Header;
